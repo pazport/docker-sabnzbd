d8052909bf556c650691cb32bca41aab331a7479
pazport <46929183+pazport@users.noreply.github.com>
2021-03-09 21:34:49 +0000

    Delete healthcheck.sh

--- healthcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILE=/config/config.ini
-
-until test -f $FILE; do sleep 1; done
-
-API=$(cat /config/config.ini | grep 'api_key =' | egrep -v 'disable|rating' | awk -F '= ' '{print $2}')
-curl -f "http://localhost:8080/sabnzbd/api?mode=queue&output=json&apikey=$API" && curl api.ipify.org || exit 1
